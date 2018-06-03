84199699c4f97f29a73beaced4126cf90f2aa065
Patross <contactpatross@gmail.com>
2018-06-03 21:49:36 +0100

    aa

--- KeepMeHealthy/KeepMeHealthy/MainActivity.cs
+++ KeepMeHealthy/KeepMeHealthy/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
+using Android.Content.Res;
+using System;
 
 namespace KeepMeHealthy
 {
@@ -13,6 +16,86 @@ namespace KeepMeHealthy
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+
+            //SELECTING SPINNERS
+            Spinner spinnerWeight = FindViewById<Spinner>(Resource.Id.spinnerWeight);
+            Spinner spinnerDistance = FindViewById<Spinner>(Resource.Id.spinnerDistance);
+
+            //ADDING VALUES TO SPINNERS;
+            List<string> listWeight = new List<string>();
+            listWeight.Add("KG");
+            listWeight.Add("POUNDS");
+
+            List<string> listDistance = new List<string>();
+            listDistance.Add("KM");
+            listDistance.Add("MILES");
+
+            ArrayAdapter adapterWeight = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, listWeight);
+            ArrayAdapter adapterDistance = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, listDistance);
+
+            spinnerWeight.Adapter = adapterWeight;
+            spinnerDistance.Adapter = adapterDistance;
+
+
+            //GETTING THE CALCULATE BUTTON
+            Button btnCalculate = FindViewById<Button>(Resource.Id.btnCalculate);
+            btnCalculate.Click += BtnCalculate_Click;
+
+            //NOW THE PROFILE BUTTON
+            ImageButton btnProfile = FindViewById<ImageButton>(Resource.Id.btnProfile);
+            btnProfile.Click += BtnProfile_Click;
+
+
+
+        }
+
+        private void BtnProfile_Click(object sender, EventArgs e)
+        {
+            this.SetContentView(Resource.Layout.Profile);
+        }
+
+        private void BtnCalculate_Click(object sender, System.EventArgs e)
+        {
+            TextView txtCaloriesBurned = FindViewById<TextView>(Resource.Id.txtCaloriesBurned);
+
+            EditText inputWeight = FindViewById<EditText>(Resource.Id.inputWeight);
+            EditText inputDistanceTravelled = FindViewById<EditText>(Resource.Id.inputDistanceTravelled);
+            EditText inputTime = FindViewById<EditText>(Resource.Id.inputTime);
+
+            if (decimal.TryParse(inputWeight.Text,out decimal weight))
+            {
+                if (decimal.TryParse(inputDistanceTravelled.Text,out decimal distanceTravelled))
+                {
+                    if (int.TryParse(inputTime.Text,out int time))
+                    {
+                        //EVERYTHING SUCCESSFULLY PARSED IN FROM THE EDIT TEXT FIELDS
+
+                        //DO THE CONVERTING
+                        Spinner spinnerWeight = FindViewById<Spinner>(Resource.Id.spinnerWeight);
+                        Spinner spinnerDistance = FindViewById<Spinner>(Resource.Id.spinnerDistance);
+
+                        const decimal POUNDTOKG = 0.453592m;
+                        const decimal MILESTOKM = 1.609m;
+
+                        if (spinnerWeight.GetItemAtPosition(0).ToString() == "POUNDS")
+                        {
+                            weight = weight * POUNDTOKG;
+                        }
+                        if (spinnerDistance.GetItemAtPosition(0).ToString() == "MILES")
+                        {
+                            distanceTravelled = distanceTravelled * MILESTOKM;
+                        }
+
+                        RadioButton radioExerciseRunning = FindViewById<RadioButton>(Resource.Id.radExerciseRunning);
+                        RadioButton radioExerciseWalking = FindViewById<RadioButton>(Resource.Id.radExerciseWalking);
+                        
+                        
+                        txtCaloriesBurned.Text = $"Weight {weight} Distance {distanceTravelled} Time {time}";
+                    }
+                }
+            }
+
         }
     }
 }
--- KeepMeHealthy/KeepMeHealthy/Resources/Resource.Designer.cs
+++ KeepMeHealthy/KeepMeHealthy/Resources/Resource.Designer.cs
@@ -45,7 +45,10 @@ namespace KeepMeHealthy
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int avatar_placeholder = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
@@ -60,8 +63,65 @@ namespace KeepMeHealthy
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f05000f
+			public const int btnCalculate = 2131034127;
+			
+			// aapt resource value: 0x7f050002
+			public const int btnProfile = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int imageButton1 = 2131034112;
+			public const int gridLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050009
+			public const int inputDistanceTravelled = 2131034121;
+			
+			// aapt resource value: 0x7f05000d
+			public const int inputTime = 2131034125;
+			
+			// aapt resource value: 0x7f050005
+			public const int inputWeight = 2131034117;
+			
+			// aapt resource value: 0x7f050012
+			public const int radExerciseRunning = 2131034130;
+			
+			// aapt resource value: 0x7f050011
+			public const int radExerciseWalking = 2131034129;
+			
+			// aapt resource value: 0x7f050010
+			public const int radioGroup1 = 2131034128;
+			
+			// aapt resource value: 0x7f050004
+			public const int space1 = 2131034116;
+			
+			// aapt resource value: 0x7f050008
+			public const int space2 = 2131034120;
+			
+			// aapt resource value: 0x7f05000c
+			public const int space3 = 2131034124;
+			
+			// aapt resource value: 0x7f05000a
+			public const int spinnerDistance = 2131034122;
+			
+			// aapt resource value: 0x7f050006
+			public const int spinnerWeight = 2131034118;
+			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView2 = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int textView3 = 2131034119;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textView4 = 2131034123;
+			
+			// aapt resource value: 0x7f05000e
+			public const int textView5 = 2131034126;
+			
+			// aapt resource value: 0x7f050013
+			public const int txtCaloriesBurned = 2131034131;
 			
 			static Id()
 			{
@@ -78,6 +138,9 @@ namespace KeepMeHealthy
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Profile = 2130903041;
 			
 			static Layout()
 			{
